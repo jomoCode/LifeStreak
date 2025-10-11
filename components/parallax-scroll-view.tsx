38bc70640b0240d9f04d8ec8c5773b7db1cbc5e3
jomoartz <johnjohananmogbolu@gmail.com>
2025-10-12 00:09:18 +0100

    removed some stuff

--- components/parallax-scroll-view.tsx
+++ components/parallax-scroll-view.tsx
@@ -11,7 +11,7 @@ import { ThemedView } from '@/components/themed-view';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { useThemeColor } from '@/hooks/use-theme-color';
 
-const HEADER_HEIGHT = 250;
+const HEADER_HEIGHT = 100;
 
 type Props = PropsWithChildren<{
   headerImage: ReactElement;
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   header: {
-    height: HEADER_HEIGHT,
+    height: 0,
     overflow: 'hidden',
   },
   content: {
