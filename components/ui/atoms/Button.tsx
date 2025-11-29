4e9682e08f2b4b416aebca64d5328a33bcd839e2
jomoartz <johnjohananmogbolu@gmail.com>
2025-11-29 11:07:17 +0100

    feat: Button

--- components/ui/atoms/Button.tsx
+++ components/ui/atoms/Button.tsx
@@ -5,16 +5,15 @@ import { Text, TouchableOpacity, useColorScheme, View } from "react-native";
 type ButtonProps = {
   onPress: () => void;
   text: string;
-  icon:
 };
 
-const Button = ({ onPress, text, icon }: ButtonProps) => {
+const Button = ({ onPress, text }: ButtonProps) => {
   const theme = useColorScheme() === "dark" ? "dark" : "light";
   const styles = buttonStyles(theme);
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={onPress} style={styles.content}>
-        <Text  style={styles.text}>{text}</Text>
+        <Text style={styles.text}>{text}</Text>
       </TouchableOpacity>
     </View>
   );
