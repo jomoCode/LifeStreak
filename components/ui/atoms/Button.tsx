6eba585db2360d1b42ff9e12e44ea1596f06c1ce
jomoartz <johnjohananmogbolu@gmail.com>
2025-11-29 11:06:54 +0100

    feat: Button

---
+++ components/ui/atoms/Button.tsx
@@ -0,0 +1,23 @@
+import { buttonStyles } from "@/lib/styles/Styles";
+import React from "react";
+import { Text, TouchableOpacity, useColorScheme, View } from "react-native";
+
+type ButtonProps = {
+  onPress: () => void;
+  text: string;
+  icon:
+};
+
+const Button = ({ onPress, text, icon }: ButtonProps) => {
+  const theme = useColorScheme() === "dark" ? "dark" : "light";
+  const styles = buttonStyles(theme);
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress} style={styles.content}>
+        <Text  style={styles.text}>{text}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export { Button };
