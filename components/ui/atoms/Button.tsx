2fa41dbbec9c423ebec00f1280c203e4ab2df043
jomoartz <johnjohananmogbolu@gmail.com>
2025-11-30 02:55:34 +0100

    task: used RNA for button

--- components/ui/atoms/Button.tsx
+++ components/ui/atoms/Button.tsx
@@ -1,21 +1,39 @@
+import { Colors } from "@/constants/theme";
 import { buttonStyles } from "@/lib/styles/Styles";
-import React from "react";
-import { Text, TouchableOpacity, useColorScheme, View } from "react-native";
+import React, { ReactNode } from "react";
+import { useColorScheme, View } from "react-native";
+import { Button as RnaButton } from "react-native-paper";
+
+type Mode =
+  | "text"
+  | "outlined"
+  | "contained"
+  | "elevated"
+  | "contained-tonal"
+  | undefined;
 
 type ButtonProps = {
   onPress: () => void;
   text: string;
+  mode?: Mode;
+  children: ReactNode;
 };
 
-const Button = ({ onPress, text }: ButtonProps) => {
+const Button = ({ onPress, text, mode, children }: ButtonProps) => {
   const theme = useColorScheme() === "dark" ? "dark" : "light";
   const styles = buttonStyles(theme);
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={onPress} style={styles.content}>
-        <Text style={styles.text}>{text}</Text>
-      </TouchableOpacity>
-    </View>
+    <RnaButton
+      onPress={onPress}
+      mode={mode}
+      rippleColor={Colors[theme].tint}
+      compact={true}
+      contentStyle={{ padding: 0 }}
+    >
+      <View style={styles.container}>
+        <View style={styles.content}>{children}</View>
+      </View>
+    </RnaButton>
   );
 };
 
