1c180687b7017e18588381a09eda729bcdbc0221
jomoartz <johnjohananmogbolu@gmail.com>
2025-12-10 22:55:10 +0100

    feat(creIndex): setup

--- app/CreateEvents/index.tsx
+++ app/CreateEvents/index.tsx
@@ -1,26 +1,59 @@
-import CreateEvent from "@/components/ui/Template/CreateEvent";
-import { useStreakForm } from "@/context/useCreateStreakForm";
+import { LsButton } from "@/components/ui/atoms/LsButton";
+import { CreateStreakFormProps } from "@/context/CreateStreakForm_";
+import { useGeneralStyles } from "@/hooks/styles/useStyles";
+import { MaterialIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import { Text, TextInput, View } from "react-native";
 
-export default function GoalTitleScreen() {
-  const streak = useStreakForm();
-const router = useRouter();
-  return (
-    <CreateEvent
-      title="Your Goal"
-      placeholder="Enter goal title"
-      fieldType="text"
-      value={streak.form.goalTitle}
-      onChange={(value) => {
-        if (typeof value !== "string") {
-          throw new Error("Invalid value type");
-        }
-        streak.setGoalTitle(value);
-      }}
-      onNext={() => {
+const EventTitle = () => {
+  const { control, getFieldState, watch } =
+    useFormContext<CreateStreakFormProps>();
+  const { error } = getFieldState("eventName");
+  const styles = useGeneralStyles();
+  const router = useRouter();
+  const moveToNextStep = () => {
     router.push("/CreateEvents/StartDate");
-      
-      }}
-    />
+  };
+
+  return (
+    <View style={styles.crePageContainer}>
+      <Text style={styles.crePageTitle}>What do you wish to achieve?</Text>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+          min: 3,
+          max: 50,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <View>
+            <Text style={styles.crePageLabel}>Write your end result</Text>
+            <TextInput
+              placeholder="Eg: Morning Jog 30mins for 10 days"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              multiline={true}
+              style={styles.crePageInput}
+            />
+          </View>
+        )}
+        name="eventName"
+      />
+      {error && <Text>{error.message}</Text>}
+      <View style={styles.crePageButtonContainer}>
+        <LsButton onPress={moveToNextStep}>
+          <MaterialIcons
+            name="arrow-forward"
+            size={35}
+            style={styles.crePageButtonIcon}
+          />
+        </LsButton>
+      </View>
+    </View>
   );
-}
+};
+
+export default EventTitle;
