f6e6c5d7f73c2646e01759420429a826b026b053
jomoartz <johnjohananmogbolu@gmail.com>
2025-12-11 12:24:59 +0100

    setup streak interval

--- app/CreateEvents/StreakInterval.tsx
+++ app/CreateEvents/StreakInterval.tsx
@@ -1,32 +1,87 @@
-import CreateEvent from "@/components/ui/Template/CreateEvent";
-import { useStreakForm } from "@/context/useCreateStreakForm";
+import { CreateEvent } from "@/components/ui/Template/CreateEvent";
+import { CreateStreakFormProps } from "@/context/CreateStreakForm_";
+import { useGeneralStyles } from "@/hooks/styles/useStyles";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { useFormContext } from "react-hook-form";
+import {
+  FlatList,
+  Pressable,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
 
-const IntervalScreen = () => {
-  const streak = useStreakForm();
+const intervalOptions = [
+  { label: "Every day", value: 1 },
+  { label: "Every 2 days", value: 2 },
+  { label: "Every 3 days", value: 3 },
+  { label: "Every 4 days", value: 4 },
+  { label: "Every 5 days", value: 5 },
+  { label: "Every 6 days", value: 6 },
+  { label: "Weekly", value: 7 },
+];
+
+const StreakDuration = () => {
+  const [showPicker, setShowPicker] = useState(false);
+  const [picked, setPicked] = useState<string>();
+  const styles = useGeneralStyles();
   const router = useRouter();
-
-  const intervalOptions = [
-    { label: "Every day", value: 1 },
-    { label: "Every 2 days", value: 2 },
-    { label: "Every 3 days", value: 3 },
-    { label: "Every 4 days", value: 4 },
-    { label: "Every 5 days", value: 5 },
-    { label: "Every 6 days", value: 6 },
-    { label: "Weekly", value: 7 },
-  ];
+  const { getValues, setValue } = useFormContext<CreateStreakFormProps>();
+  const handleSelect = (value: number) => {
+    setValue("interval", value);
+    setShowPicker(false);
+    setPicked(`${value} days interval selected`);
+  };
+  const nextStep = () => {
+    // Validate value
+    const value = Number(getValues("interval"));
+    if (typeof value != "number")
+      throw new Error("duration should be a number");
+    if (!value || value < 1) throw new Error("interval must be atleast 1 days");
+    if (value > 7) throw new Error("interval must be at most 7 days");
+    // Update value with validated value before routing
+    setValue("interval", value);
+    router.push("/CreateEvents/TimesPerDay");
+  };
 
   return (
     <CreateEvent
-      title="Frequency"
-      fieldType="select"
-      value={streak.form.interval}
-      options={intervalOptions}
-      onChange={(v) => streak.setInterval(v as number)}
-      onPrev={() => router.back()}
-      onNext={() => router.push("/CreateEvents/TimesPerDay")}
-    />
+      field="duration"
+      moveToNextStep={nextStep}
+      title="How long is your streak?"
+    >
+      <Pressable
+        onPress={() => {
+          setShowPicker(!showPicker);
+        }}
+      >
+        <Text style={styles.crePageLabel}>
+          <Text>Select number Interval</Text>{" "}
+          <MaterialCommunityIcons
+            name={showPicker ? "menu-down" : "menu-right"}
+            size={20}
+          />
+        </Text>
+      </Pressable>
+      {!showPicker && picked && <Text style={styles.title}>{picked}</Text>}
+      <View style={{ minHeight: 250 }}>
+        {showPicker && (
+          <FlatList
+            data={intervalOptions}
+            keyExtractor={(item) => item.label}
+            ItemSeparatorComponent={() => <View style={{ height: 5 }} />}
+            renderItem={(item) => (
+              <TouchableHighlight onPress={() => handleSelect(item.item.value)}>
+                <Text style={styles.crePageInput}>{item.item.label}</Text>
+              </TouchableHighlight>
+            )}
+          />
+        )}
+      </View>
+    </CreateEvent>
   );
-}
+};
 
-export default IntervalScreen;
+export default StreakDuration;
