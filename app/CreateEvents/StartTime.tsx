a0adff20edb99d742b0c1520681c1c05cbf752d6
jomoartz <johnjohananmogbolu@gmail.com>
2025-12-11 10:12:22 +0100

    setup starttime

--- app/CreateEvents/StartTime.tsx
+++ app/CreateEvents/StartTime.tsx
@@ -1,26 +1,71 @@
-import CreateEvent from "@/components/ui/Template/CreateEvent";
-import { useStreakForm } from "@/context/useCreateStreakForm";
-import { useRouter } from "expo-router";
+import { ActionButton } from "@/components/ui/atoms/ActionButton";
+import { CreateEvent } from "@/components/ui/Template/CreateEvent";
+import { CreateStreakFormProps } from "@/context/CreateStreakForm_";
+import { useGeneralStyles } from "@/hooks/styles/useStyles";
+import { useColors } from "@/hooks/useColors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
+import { useState } from "react";
+import { useFormContext } from "react-hook-form";
+import { Text } from "react-native";
 
-export default function StartTimeScreen() {
-  const streak = useStreakForm();
-  const router = useRouter();
-
+const StartTimeScreen = () => {
+  const [calendarValue, setCalendarValue] = useState<Date>();
+  const [calendar, setCalendar] = useState(false);
+  const color = useColors();
+  const { setValue } = useFormContext<CreateStreakFormProps>();
+  const styles = useGeneralStyles();
+  const setDate = (event: DateTimePickerEvent, date?: Date) => {
+    if (!date) {
+      console.error("No date selected: ");
+      return null;
+    }
+    if (date) setCalendarValue(date);
+    setCalendar(false);
+    const time = date.getTime().toString();
+    const timeArray = time.split(":");
+    if (timeArray[0].length === 1) timeArray[0] = "0" + timeArray[0];
+    const formattedTime = `${timeArray[0]}:${timeArray[1]}`;
+    console.log("final selected time: ", formattedTime);
+    setValue("startTime", formattedTime);
+  };
   return (
-    <CreateEvent
-      title="Start Time"
-      fieldType="time"
-      value={
-        streak.form.startTime
-          ? new Date(`1970-01-01T${streak.form.startTime}:00`)
-          : new Date()
-      }
-      onChange={(d) => {
-        if (!(d instanceof Date)) throw new Error("Invalid time value");
-        streak.setStartTime(d);
-      }}
-      onPrev={() => router.back()}
-      onNext={() => router.push("/CreateEvents/StreakInterval")}
-    />
+    <CreateEvent title="What time?" moveToNextStep={() => ""} field="startTime">
+      {!calendar ? (
+        <ActionButton
+          containerStyles={{ alignItems: "center" }}
+          handleSubmit={() => {
+            setCalendar(true);
+          }}
+        />
+      ) : (
+        <DateTimePicker
+          mode="time"
+          value={calendarValue ? calendarValue : new Date()}
+          onChange={setDate}
+        />
+      )}
+
+      <Text style={styles.crePageLabel}>
+        Selected Time:{" "}
+        <Text style={{ color: "black", fontSize: 16 }}>
+          {calendarValue ? calendarValue.toLocaleTimeString() : "-- -- --"}
+        </Text>
+      </Text>
+      <Text>
+        {calendarValue && (
+          <MaterialCommunityIcons
+            name="check-outline"
+            color={color.background}
+            size={25}
+            style={{ textAlign: "center" }}
+          />
+        )}
+      </Text>
+    </CreateEvent>
   );
-}
+};
+
+export default StartTimeScreen;
