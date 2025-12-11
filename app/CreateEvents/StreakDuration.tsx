a52da18ad134cad740fefb61a7ad2de462a9d290
jomoartz <johnjohananmogbolu@gmail.com>
2025-12-11 10:57:47 +0100

    streakDuration

--- app/CreateEvents/StreakDuration.tsx
+++ app/CreateEvents/StreakDuration.tsx
@@ -1,29 +1,61 @@
-import CreateEvent from "@/components/ui/Template/CreateEvent";
-import { useStreakForm } from "@/context/useCreateStreakForm";
+import { CreateEvent } from "@/components/ui/Template/CreateEvent";
+import { CreateStreakFormProps } from "@/context/CreateStreakForm_";
+import { useGeneralStyles } from "@/hooks/styles/useStyles";
 import { useRouter } from "expo-router";
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import { Text, TextInput, View } from "react-native";
 
-export default function DurationScreen() {
-  const streak = useStreakForm();
+const validationRules = {
+  required: true,
+  min: 1,
+  max: 50,
+};
+
+const EventTitle = () => {
+  const styles = useGeneralStyles();
   const router = useRouter();
+  const { control, getValues, setValue } =
+    useFormContext<CreateStreakFormProps>();
+  const nextStep = () => {
+    // Validate value
+    const value = Number(getValues("duration"));
+    if (typeof value != "number")
+      throw new Error("duration should be a number");
+    if (!value || value <= 1) throw new Error("streak must be atleast 2 days");
+    if (value > 30) throw new Error("Streak must be at most 30days");
+    // Update value with validated value before routing
+    setValue("duration", value);
+    router.push("/CreateEvents/StreakInterval");
+  };
 
   return (
     <CreateEvent
+      field="duration"
+      moveToNextStep={nextStep}
       title="How long is your streak?"
-      placeholder=""
-      fieldType="number"
-      value={streak.form.duration}
-      onChange={(d) => {
-        if (typeof d !== "number") {
-          throw new Error("Invalid value type");
-        }
-        streak.setDuration(d);
-      }}
-      onPrev={() => {
-        router.back();
-      }}
-      onNext={() => {
-        router.push("/CreateEvents/StreakInterval");
-      }}
-    />
+    >
+      <Controller
+        control={control}
+        rules={validationRules}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <View>
+            <Text style={styles.crePageLabel}>How long to achieve goal</Text>
+            <TextInput
+              placeholder="Eg: 28"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={`${value}`}
+              keyboardType="phone-pad"
+              multiline={false}
+              style={styles.crePageInput}
+            />
+          </View>
+        )}
+        name="duration"
+      />
+    </CreateEvent>
   );
-}
+};
+
+export default EventTitle;
