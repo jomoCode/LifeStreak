6b38083496538992a653e30617589b11454fa299
jomoartz <johnjohananmogbolu@gmail.com>
2025-12-11 21:00:18 +0100

    error handling index(create event)

--- app/CreateEvents/index.tsx
+++ app/CreateEvents/index.tsx
@@ -2,7 +2,7 @@ import { CreateEvent } from "@/components/ui/Template/CreateEvent";
 import { CreateStreakFormProps } from "@/context/CreateStreakForm_";
 import { useGeneralStyles } from "@/hooks/styles/useStyles";
 import { useRouter } from "expo-router";
-import React from "react";
+import React, { useState } from "react";
 import { Controller, useFormContext } from "react-hook-form";
 import { Text, TextInput, View } from "react-native";
 
@@ -14,14 +14,24 @@ const validationRules = {
 
 const EventTitle = () => {
   const styles = useGeneralStyles();
+  const [err, setErr] = useState<string | null>(null);
   const router = useRouter();
   const { control, getValues } = useFormContext<CreateStreakFormProps>();
   const nextStep = () => {
-    const event = getValues('eventName');
-    if (!event) throw new Error('Streak title is required');
-    if (event.length <= 1) throw new Error('streak title must be atleast 4 characters long');
-    if (event.length > 50) throw new Error('Event name must be most 50 characters long')
-    router.push("/CreateEvents/StartDate");
+    try {
+      const event = getValues("eventName");
+      if (!event) throw new Error("Streak title is required");
+      if (event.length <= 1)
+        throw new Error("streak title must be atleast 4 characters long", {
+          cause: "Invalid client input",
+        });
+      if (event.length > 50)
+        throw new Error("Event name must be most 50 characters long");
+      router.push("/CreateEvents/StartDate");
+    } catch (error: unknown | Error) {
+      const errorObj = error as Error;
+      setErr(errorObj?.message);
+    }
   };
 
   return (
@@ -37,13 +47,17 @@ const EventTitle = () => {
           <View>
             <Text style={styles.crePageLabel}>Write your end result</Text>
             <TextInput
-              placeholder="Eg: Morning Jog 30mins for 10 days"
+              placeholder="Eg: Morning Jog for 10 days"
               onBlur={onBlur}
-              onChangeText={onChange}
+              onChangeText={(event) => {
+                onChange(event);
+                setErr(null);
+              }}
               value={value}
               multiline={true}
               style={styles.crePageInput}
             />
+            {err && <Text style={{ color: "red" }}>{err}</Text>}
           </View>
         )}
         name="eventName"
