15f7ac4fdfb1326e4cc0087096241e79af3e3687
jomoartz <johnjohananmogbolu@gmail.com>
2025-12-11 21:41:29 +0100

    feat: error states(StartDuration)

--- app/CreateEvents/StreakDuration.tsx
+++ app/CreateEvents/StreakDuration.tsx
@@ -2,7 +2,7 @@ import { CreateEvent } from "@/components/ui/Template/CreateEvent";
 import { CreateStreakFormProps } from "@/context/CreateStreakForm_";
 import { useGeneralStyles } from "@/hooks/styles/useStyles";
 import { useRouter } from "expo-router";
-import React from "react";
+import React, { useState } from "react";
 import { Controller, useFormContext } from "react-hook-form";
 import { Text, TextInput, View } from "react-native";
 
@@ -15,18 +15,25 @@ const validationRules = {
 const StreakDuration = () => {
   const styles = useGeneralStyles();
   const router = useRouter();
+  const [err, setErr] = useState<string | null>(null);
   const { control, getValues, setValue } =
     useFormContext<CreateStreakFormProps>();
   const nextStep = () => {
-    // Validate value
-    const value = Number(getValues("duration"));
-    if (typeof value != "number")
-      throw new Error("duration should be a number");
-    if (!value || value <= 1) throw new Error("streak must be atleast 2 days");
-    if (value > 30) throw new Error("Streak must be at most 30days");
-    // Update value with validated value before routing
-    setValue("duration", value);
-    router.push("/CreateEvents/StreakInterval");
+    try {
+      // Validate value
+      const value = Number(getValues("duration"));
+      if (typeof value != "number")
+        throw new Error("duration should be a number");
+      if (!value || value <= 1)
+        throw new Error("streak must be atleast 2 days");
+      if (value > 30) throw new Error("Streak must be at most 30days");
+      // Update value with validated value before routing
+      setValue("duration", value);
+      router.push("/CreateEvents/StreakInterval");
+    } catch (error: unknown | Error) {
+      const errorObj = error as Error;
+      setErr(errorObj?.message);
+    }
   };
 
   return (
@@ -44,12 +51,16 @@ const StreakDuration = () => {
             <TextInput
               placeholder="Eg: 28"
               onBlur={onBlur}
-              onChangeText={onChange}
+              onChangeText={(event) => {
+                onChange(event);
+                setErr(null);
+              }}
               value={`${value}`}
               keyboardType="phone-pad"
               multiline={false}
               style={styles.crePageInput}
             />
+            {err && <Text style={{ color: "red" }}>{err}</Text>}
           </View>
         )}
         name="duration"
