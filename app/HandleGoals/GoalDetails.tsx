004f794d8f3b1b236bafdf569e83f5e2b5226d6c
jomoartz <johnjohananmogbolu@gmail.com>
2025-12-15 21:52:44 +0100

    filter and get room data.. room detail page

--- app/HandleGoals/GoalDetails.tsx
+++ app/HandleGoals/GoalDetails.tsx
@@ -1,12 +1,19 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { useDb } from "@/context/useBackend";
+import { useLocalSearchParams } from "expo-router/build/hooks";
+import React from "react";
+import { Text, View } from "react-native";
 
 const GoalDetails = () => {
+  const { data, refresh } = useDb();
+  const { goalId } = useLocalSearchParams<{ goalId: string }>();
+  const roomData = data.find((data) => data.event_id === goalId);
+  console.log("ROOM DATA:", roomData);
+
   return (
     <View>
       <Text>GoalDetails</Text>
     </View>
-  )
-}
+  );
+};
 
 export default GoalDetails;
