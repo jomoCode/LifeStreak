9f86d9492d3500a24bc660f1c96ef2b576728590
jomoartz <johnjohananmogbolu@gmail.com>
2025-12-22 17:18:42 +0100

    update(Text): added new prop - align

--- components/ui/atoms/Title.tsx
+++ components/ui/atoms/Title.tsx
@@ -6,9 +6,10 @@ type TitleProps = {
   children: ReactNode;
   variant: "lg" | "med" | "sm";
   color?: string;
+  align?: "center" | "left" | "right";
 };
 
-const Title = ({ children, variant, color }: TitleProps) => {
+const Title = ({ children, variant, color, align }: TitleProps) => {
   const style = useTextStyles();
   const textSize = variant === "lg" ? 30 : variant === "med" ? 20 : 16;
   return (
@@ -24,15 +25,17 @@ const Title = ({ children, variant, color }: TitleProps) => {
   );
 };
 
-const LsText = ({ children, variant, color }: TitleProps) => {
+const LsText = ({ children, variant, color, align }: TitleProps) => {
   const style = useTextStyles();
   const textSize = variant === "lg" ? 25 : variant === "med" ? 18 : 14;
+
   return (
     <Text
       style={[
         style.lsText,
         { fontSize: textSize },
-        color ? { color: color } : {},
+        color ? { color: color } : null,
+        align ? { textAlign: align } : null,
       ]}
     >
       {children}
