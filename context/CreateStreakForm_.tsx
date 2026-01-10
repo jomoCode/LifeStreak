652edd05dff2b1eeb5f3f5bb4e4671f68943e3e2
jomoartz <johnjohananmogbolu@gmail.com>
2026-01-10 10:17:08 +0100

    update(CreateStreakForm)

--- context/CreateStreakForm_.tsx
+++ context/CreateStreakForm_.tsx
@@ -1,6 +1,17 @@
-import { CreateEvent } from "@/lib/CRUDE_sqlite";
+import { Schedule } from "@/types";
 import { ReactNode } from "react";
 import { FormProvider, useForm } from "react-hook-form";
+
+type CreateEvent = {
+  name: string;
+  schedule: Schedule; 
+  startDate: Date | number;
+  endDate?: Date | number;
+  totalDays?: number; // maximum 30
+  startTime: string;
+  endTime: string;
+};
+
 
 export type CreateStreakFormProps = CreateEvent;
 type CreateStreakFormKids = { children: ReactNode };
@@ -9,3 +20,4 @@ export const CreateStreakForm = ({ children }: CreateStreakFormKids) => {
   const methods = useForm<CreateStreakFormProps>({});
   return <FormProvider {...methods}>{children}</FormProvider>;
 };
+
